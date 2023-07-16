d73ef3206c343a91e8d67dbba3ef0973aafd0ae0
rush1016 <russelmicotamayo@gmail.com>
2023-07-16 10:19:53 +0800

    Fixed UI for Login, Register, Forgot, OTP

--- read_project/settings.py
+++ read_project/settings.py
@@ -124,9 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'read_project/static/')
+   os.path.join(BASE_DIR, 'read_app/static/')
 ]
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
